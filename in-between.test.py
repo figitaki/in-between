568d40030528f4c6115f69ef0ff17f69771b70f7
Carey Janecka <figitaki96@yahoo.com>
2024-04-21 20:27:56 -0600

    add update function refactor stuff

---
+++ in-between.test.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class InBetweenSimulationTest():
+    def test_game_creation(self):
+        # TODO: setup game
+        self.assertEqual(1, 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
